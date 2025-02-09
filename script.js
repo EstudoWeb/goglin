ee361188385c79044ac514e4b4c15de80fd5954f
GOLDNFT <goldnftoficial@gmail.com>
2025-02-09 19:08:36 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let valueForSearch = document.getElementById('valuesearch')
+let search = document.getElementById('searchButton')
+let resForSearch = document.getElementById('res')
+
+search.addEventListener('click', ()=>createSearch(valueForSearch.value))
+
+const createSearch=(value)=>{
+  if(value.replace(/\D/g, "").length != 8){
+    resForSearch.innerHTML = "<h2 style='font-family: monospace'>Esse cep não tem 8 digítos!</h2>"
+  }else{
+    resForSearch.innerHTML = "<h2>Carregando</h2>"
+    fetch(`https://viacep.com.br/ws/${value.replace(/\D/g, "")}/json/`)
+    .then(res => res.json())
+    .then(dados=>{
+      if(dados.erro != "true"){
+        res.innerHTML = `<h2>Resultado da consulta 🏠:</h2><p><strong>Sua consulta foi um sucesso ✅!</strong></p><p><strong>Cep:</strong> ${dados.cep}<br><strong>Estado:</strong> ${dados.estado}<br><strong>Sigla:</strong> ${dados.uf}<br><strong>Região:</strong> ${dados.regiao}<br><strong>DDD:</strong> ${dados.ddd}<br><strong>Localidade:</strong> ${dados.localidade}<br><strong>Bairro:</strong> ${dados.bairro}<br><strong>Logradouro:</strong> ${dados.logradouro} <br><br><a href='https://viacep.com.br/ws/${value.replace(/\D/g, "")}/json/' target='_blank'><strong>Mais informações em JSON</strong></a></p>`
+      }else{
+        res.innerHTML = `<br>Não foi encontrado um local com esse cep!`
+      }
+    }).catch((err)=>{
+      res.innerHTML = `<h2>Sem conexão com o servidor!</h2>`
+      console.error(err)
+    })
+}
+}
